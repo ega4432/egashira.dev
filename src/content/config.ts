e2ef1b04f11506c7e9a2092a3956a2059aa4fe7a
ega4432 <38056766+ega4432@users.noreply.github.com>
2023-02-09 01:34:00 +0900

    refactor: exec `npm run lint` and `npm run format`

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,5 +2,5 @@ import { defineCollection } from "astro:content";
 import { noteScheme } from "./schemes";
 
 export const collections = {
-	notes: defineCollection(noteScheme)
+  notes: defineCollection(noteScheme)
 };
