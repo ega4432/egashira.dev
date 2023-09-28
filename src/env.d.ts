35c8e4e2a450c5ca02ed77192cdad603056bc84f
ega4432 <38056766+ega4432@users.noreply.github.com>
2023-09-28 10:55:05 +0900

    feat: add ad

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-import { GtmId } from "../src/components/GoogleTagManager.astro";
+import { type GtmId } from "../src/components/GoogleTagManager.astro";
 
 interface ImportMetaEnv {
   readonly GTM_ID: GtmId;
+  readonly AD_CLIENT: string;
+  readonly AD_SLOT: string;
 }
 
 interface ImportMeta {
