0765b8bc204bd0ee35a34a2bf647275dce1b9d23
ega4432 <38056766+ega4432@users.noreply.github.com>
2023-09-30 12:18:06 +0900

    fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,13 +2,22 @@
 /// <reference types="astro/client" />
 
 import { type GtmId } from "../src/components/GoogleTagManager.astro";
+import { type ADClient } from "./components/GoogleAdsense.astro";
 
 interface ImportMetaEnv {
   readonly GTM_ID: GtmId;
-  readonly AD_CLIENT: string;
+  readonly AD_CLIENT: ADClient;
   readonly AD_SLOT: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
+}
+
+interface Window {
+  adsbygoogle?: { [key: string]: unknown }[];
+}
+
+declare global {
+  const window: Window;
 }
