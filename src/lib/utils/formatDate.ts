f0b2d0f86f0404193e80461511c0d27f0703ce63
ega4432 <38056766+ega4432@users.noreply.github.com>
2023-12-28 09:58:10 +0900

    chore: remove deps

--- src/lib/utils/formatDate.ts
+++ src/lib/utils/formatDate.ts
@@ -1,9 +1,9 @@
-import { formatInTimeZone } from "date-fns-tz";
+// import { formatInTimeZone } from "date-fns-tz";
 
-export const formatDate = (
-  date: string,
-  timeZone: string = "Asia/Tokyo",
-  formatStr: string = "yyyy年MM月dd日"
-) => {
-  return formatInTimeZone(new Date(date), timeZone, formatStr);
-};
+// export const formatDate = (
+//   date: string,
+//   timeZone: string = "Asia/Tokyo",
+//   formatStr: string = "yyyy年MM月dd日"
+// ) => {
+//   return formatInTimeZone(new Date(date), timeZone, formatStr);
+// };
